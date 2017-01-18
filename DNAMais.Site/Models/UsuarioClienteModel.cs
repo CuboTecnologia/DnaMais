9709dce46215d8cb18f9085387dd993839f7d7df
CuboTecnologia <claudio@info68.com.br>
2017-01-18 15:31:38 -0200

    update 2017-01-18 15:31

---
+++ DNAMais.Site/Models/UsuarioClienteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DNAMais.Site.Models
+{
+    public class UsuarioClienteModel
+    {
+        public int? Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public bool Master { get; set; }
+    }
+}
